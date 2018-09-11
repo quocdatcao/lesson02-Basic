efdaac33fe29f78e5d0f421863d0a513fb05cf03
Quoc Dat <nguoitoiyeu0702@gmail.com>
2018-09-11 19:20:03 +0700

    tinh tong 100 so bang for

---
+++ src/dat/dev/Basictinhtongfor.java
@@ -0,0 +1,14 @@
+package dat.dev;
+
+public class Basictinhtongfor {
+	public static void main (String[] agrs) {
+		int i;
+		int sum = 0;
+		for (i = 0; i<=100;i++) {
+			sum = sum +i;
+		}
+		System.out.println("Tong 100 so la : "+sum);
+	
+	}
+
+}
