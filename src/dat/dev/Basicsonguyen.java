dcc285b3407fffbe31fc492474a3be6d265c7fd5
Quoc Dat <nguoitoiyeu0702@gmail.com>
2018-09-11 19:11:30 +0700

    Tong hieu tich thuong so nguyen

---
+++ src/dat/dev/Basicsonguyen.java
@@ -0,0 +1,14 @@
+package dat.dev;
+
+
+public class Basicsonguyen {
+	public static void main(String[] agrs) {
+		 int a = 8;
+		 int b = 6;
+	     System.out.println("Tong cua 2 so a va b la: "+(a + b));
+	     System.out.println("Hieu cua 2 so a va b la: "+(a - b));
+	     System.out.println("Tich cua 2 so a va b la: "+(a * b));
+	     System.out.println("Thuong cua 2 so a va b la: "+(a / b));
+	}
+
+}
